e5715d7ff2eecc6e6ae6a9a866b5581ca41c8fc2
robert-candy <candyrobertt@gmail.com>
2020-05-22 00:19:15 +0300

    first commit

--- Letters.py
+++ Letters.py
@@ -1,1 +1,11 @@
 
+user_input = input("please write a sentence :  ")
+
+letter_freq = {} 
+  
+for i in user_input: 
+    if i in letter_freq: 
+        letter_freq[i] += 1
+    else: 
+        letter_freq[i] = 1
+print (letter_freq)
